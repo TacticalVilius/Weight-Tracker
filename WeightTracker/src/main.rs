f35e8ca9a6b0d571fe31029bbbc97e49a3cbcb08
TacticalVilius <viliuska@gmail.com>
2015-02-14 15:48:16 +0100

    Added date functions. Printing a number of dates when inserting weights.

--- WeightTracker/src/main.rs
+++ WeightTracker/src/main.rs
@@ -1,12 +1,7 @@
 #![feature(io)]
-#![feature(collections)]
 
 use std::old_io;
-
-struct WeightAtDate {
-    date: String,
-    weight: f32
-}
+use std::collections::HashMap;
 
 enum Action {
     InputWeights,
@@ -15,7 +10,7 @@ enum Action {
 }
 
 fn main() {
-    let mut weights = Vec::<WeightAtDate>::new();
+    let mut weights = HashMap::new();
     
     loop {
         let user_input = old_io::stdin().read_line().ok().expect("Failed to read input");
@@ -35,22 +30,106 @@ fn process_input(input: &str) -> Action {
     }
 }
 
-fn input_weights(weights: &mut Vec<WeightAtDate>) {
+fn input_weights(weights: &mut HashMap<String, f32>) {
     let mut date = "2015.02.11".to_string();
     loop {
-        print!("{}\t", date);
+        print_weights(&date, weights, 4);
         let user_input = old_io::stdin().read_line().ok().expect("Failed to read input");
-        let weight = validate_weight_input(user_input.trim().parse::<f32>());
+        let weight = validate_weight_input(user_input.trim().parse::<f32>().ok());
         match weight {
-            Some(weight) => weights.push(WeightAtDate {date: date.clone(), weight: weight}),
+            Some(weight) => { weights.insert(date.clone(), weight); },
             None => {
                 if valid_date_input(&user_input) { date = user_input.trim().to_string(); }
             }
         }
         println!("\nWeights inserted so far:\n");
-        for element in weights.iter() { println!("{}: {}", element.date, element.weight); }
+        for (key, value) in weights.iter() {
+            println!("{}: {}", key, value);
+        }
         println!("");
     }
+}
+
+fn print_weights(date: &str, weights: &HashMap<String, f32>, pad_num: u32) {
+    let (year, month, day) = date_from_str(date);
+    let (mut year_, mut month_, mut day_) = subtract_days_from_date(year, month, day, pad_num);
+    
+    for _ in 0..(pad_num * 2 + 1) {
+        let date_str = str_from_date(year_, month_, day_);
+        let weight = match weights.get(&date_str) {
+            Some(weight) => weight.to_string(),
+            None => "".to_string()
+        };
+        println!("{}:\t{}", date_str, weight);
+        let (new_year, new_month, new_day) = add_days_to_date(year_, month_, day_, 1);
+        year_ = new_year;
+        month_ = new_month;
+        day_ = new_day;
+    }
+}
+
+fn add_days_to_date(mut year: i32, mut month: u8, mut day: u8, num_days: u32) -> (i32, u8, u8) {
+    for _ in 0..num_days {
+        if valid_day_in_month(day + 1, month) { day = day + 1; }
+        else if valid_month(month + 1) {
+            month = month + 1;
+            day = 1;
+        }
+        else {
+            year = year + 1;
+            month = 1;
+            day = 1;
+        }
+    }
+    (year, month, day)
+}
+
+fn subtract_days_from_date(mut year: i32, mut month: u8, mut day: u8, num_days: u32) -> (i32, u8, u8) {
+    for _ in 0..num_days {
+        if valid_day_in_month(day -1, month) { day = day - 1; }
+        else if valid_month(month - 1) {
+            month = month - 1;
+            day = get_last_day_in_month(month);
+        }
+        else {
+            year = year - 1;
+            month = 12;
+            day = 31;
+        }
+    }
+    (year, month, day)
+}
+
+fn valid_day_in_month(day: u8, month: u8) -> bool {
+    day > 0 && day <= get_last_day_in_month(month)
+}
+
+fn get_last_day_in_month(month: u8) -> u8 {
+    match month {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        4 | 6 | 9 | 11 => 30,
+        2 => 29,
+        _ => 0
+    }
+}
+
+fn valid_month(month: u8) -> bool {
+    return month > 0 && month < 13
+}
+
+fn date_from_str(date: &str) -> (i32, u8, u8) {
+    let parts: Vec<&str> = date.split('.').collect();
+    (parts[0].parse::<i32>().ok().expect(""), parts[1].parse::<u8>().ok().expect(""), parts[2].parse::<u8>().ok().expect(""))
+}
+
+fn str_from_date(year: i32, month: u8, day: u8) -> String {
+    let year_str = year.to_string();
+    let mut month_str = month.to_string();
+    if month < 10 { month_str = format!("{}{}", "0", month_str); }
+    let mut day_str = day.to_string();
+    if day < 10 { day_str = format!("{}{}", "0", day_str); }
+    
+    format!("{}{}{}{}{}", year_str, ".", month_str, ".", day_str)
 }
 
 fn validate_weight_input(input: Option<f32>) -> Option<f32> {
@@ -68,7 +147,7 @@ fn valid_date_input(date_input: &str) -> bool {
     if parts.len() != 3 { return false; }
     
     if !correct_year_input(parts[0]) { return false; }
-    let month = match validate_month_input(parts[1].parse::<u8>()) {
+    let month = match validate_month_input(parts[1].parse::<u8>().ok()) {
         Option::Some(month) => month,
         Option::None => return false
     };
@@ -78,25 +157,20 @@ fn valid_date_input(date_input: &str) -> bool {
 }
 
 fn correct_day_input(day_input: &str, month: u8) -> bool {
-    let day = match day_input.parse::<u8>() {
+    let day = match day_input.parse::<u8>().ok() {
         Option::Some(day) => day,
         Option::None => return false
     };
     
     if day < 1 { return false; }
     
-    match month {
-        1 | 3 | 5 | 7 | 8 | 10 | 12 => day <= 31,
-        4 | 6 | 9 | 11 => day <= 30,
-        2 => day <= 29,
-        _ => false
-    }
+    return valid_day_in_month(day, month);
 }
 
 fn validate_month_input(month_input: Option<u8>) -> Option<u8> {
     match month_input {
         Some(month) => {
-            if month > 0 && month < 13 { return Some(month); }
+            if valid_month(month) { return Some(month); }
             else { return None; }
         },
         None => return None
@@ -104,7 +178,7 @@ fn validate_month_input(month_input: Option<u8>) -> Option<u8> {
 }
 
 fn correct_year_input(year_input: &str) -> bool {
-    match year_input.parse::<i32>() {
+    match year_input.parse::<i32>().ok() {
         Option::Some(_) => true,
         Option::None => false
     }
